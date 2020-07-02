b33a71b277ae5fb3222f2f51b633b86c1e208f64
Mustapha fil <mustapha.filale@gmail.com>
2020-07-02 20:24:54 +0100

    Adding Aureka Client of all other microservices

--- movie-info-service/src/main/java/com/movies/microservices/movieinfoservice/MovieInfoServiceApplication.java
+++ movie-info-service/src/main/java/com/movies/microservices/movieinfoservice/MovieInfoServiceApplication.java
@@ -2,9 +2,11 @@ package com.movies.microservices.movieinfoservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-public class MovieInfoServiceApplication {
+@EnableEurekaClient
+class MovieInfoServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MovieInfoServiceApplication.class, args);
