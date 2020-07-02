55c649d4b23b4625d8789e2baf7e99e189362332
Mustapha fil <mustapha.filale@gmail.com>
2020-07-02 15:55:38 +0100

    Use WebClient and deprecate RestTemplate

--- movie-catalogue-service/src/main/java/com/movies/microservices/moviecatalogueservice/MovieCatalogueServiceApplication.java
+++ movie-catalogue-service/src/main/java/com/movies/microservices/moviecatalogueservice/MovieCatalogueServiceApplication.java
@@ -2,9 +2,22 @@ package com.movies.microservices.moviecatalogueservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 public class MovieCatalogueServiceApplication {
+	
+	@Bean
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
+	}
+	
+	
+	public WebClient.Builder getWebClientBuilderr(){
+		return WebClient.builder();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MovieCatalogueServiceApplication.class, args);
--- movie-catalogue-service/src/main/java/com/movies/microservices/moviecatalogueservice/controller/MovieCatalogueResource.java
+++ movie-catalogue-service/src/main/java/com/movies/microservices/moviecatalogueservice/controller/MovieCatalogueResource.java
@@ -5,21 +5,29 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import org.springframework.http.HttpMethod;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import com.movies.microservices.moviecatalogueservice.model.CatalogueItem;
 import com.movies.microservices.moviecatalogueservice.model.Movie;
 import com.movies.microservices.moviecatalogueservice.model.Rating;
 
+import reactor.core.publisher.Mono;
+
 @RestController
 @RequestMapping("/catalog")
 public class MovieCatalogueResource implements Serializable {
 
+	@Autowired
+	private RestTemplate restTemplate;
+
+	@Autowired
+	private WebClient.Builder webClientBuilder;
 	/**
 	 * 
 	 */
@@ -28,13 +36,21 @@ public class MovieCatalogueResource implements Serializable {
 	@GetMapping("/movie/{userId}")
 	public List<CatalogueItem> getCatalog(@PathVariable("userId") String userId) {
 
-		RestTemplate restTemplate = new RestTemplate();
+		// RestTemplate restTemplate = new RestTemplate();
+		// WebClient.Builder builder=WebClient.builder();
 
 		List<Rating> listRatings = Arrays.asList(new Rating("1232", 5), new Rating("1233", 4), new Rating("1234", 3),
 				new Rating("1235", 2), new Rating("1236", 1));
 
 		return listRatings.stream().map(rating -> {
-			Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
+			// Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" +
+			// rating.getMovieId(), Movie.class);
+			
+		Movie movie = webClientBuilder.build()
+				.get()
+				.uri("http://localhost:8082/movies/" + rating.getMovieId())
+				.retrieve()
+				.bodyToMono(Movie.class).block();
 			return new CatalogueItem(movie.getName(), "DESC", rating.getRating());
 		}).collect(Collectors.toList());
 
