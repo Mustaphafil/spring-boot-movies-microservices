4d9894836ac6e9e3414c0a6424fe3cfb77c1bb61
Mustapha fil <mustapha.filale@gmail.com>
2020-07-02 10:34:42 +0100

    Create First Services

---
+++ movie-catalogue-service/src/test/java/com/movies/microservices/moviecatalogueservice/MovieCatalogueServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.movies.microservices.moviecatalogueservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieCatalogueServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
